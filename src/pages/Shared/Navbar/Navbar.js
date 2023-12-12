7c40f473aa9cda6304728c243b6ab3f01571600c
Waheed-Labib <labibwahid28@gmail.com>
2023-12-12 10:48:16 +0600

    updated navbar

--- src/pages/Shared/Navbar/Navbar.js
+++ src/pages/Shared/Navbar/Navbar.js
@@ -1,6 +1,7 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { GiMusicalScore } from "react-icons/gi";
+import { RiArrowDropDownLine } from "react-icons/ri";
 import { FaUser } from "react-icons/fa";
 import { IoSunny } from "react-icons/io5";
 import { MdDarkMode } from "react-icons/md";
@@ -9,90 +10,55 @@ import { ThemeContext } from '../../../contexts/ThemeProvider/ThemeProvider';
 
 const Navbar = () => {
     const { mode, setMode } = useContext(ThemeContext)
+    const [catalogHover, setCatalogHover] = useState(false);
 
     useEffect(() => {
         localStorage.setItem('classica-theme', mode)
     }, [mode])
 
     return (
-        <div className="navbar bg-accent text-base-100 h-24 p-4 pt-8">
 
-            {/* navbar left end */}
-            <div className="navbar-start">
-
+        <div className='flex justify-between items-center bg-accent text-base-100 h-32 p-8 pt-8 text-xl'>
+            {/* navbar left */}
+            <div>
                 {/* mobile device dropdown */}
-                <div className="dropdown">
-                    {/* dropdown icon */}
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </div>
-                    {/* menu */}
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link to='/'>Home</Link></li>
-                        <li>
-                            <p>Catalog</p>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><Link>Blog</Link></li>
-                    </ul>
-                </div>
 
-                {/* navbar logo at left */}
-                <Link to='/' className="btn btn-ghost text-4xl">
+                {/* brand logo */}
+                <Link to='/' className="flex items-center gap-1 text-3xl">
                     <GiMusicalScore></GiMusicalScore>
-                    <p className='ml-2 logo-text font-thin'>Classica</p>
+                    <p className='ml-2 logo-text font-thin text-4xl'>Classica</p>
                 </Link>
+
             </div>
 
             {/* navbar center */}
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li className={`menu-option-${mode}-mode`}><Link to='/'>Home</Link></li>
-                    <li className='dropdown-menu'>
-                        <details>
-                            <summary>Catalog</summary>
-                            <ul className="bg-base-100 text-accent rounded-sm p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li className={`menu-option-${mode}-mode`}><Link>Blog</Link></li>
+            <div className={`flex items-center gap-10 nav-center-${mode}-mode`}>
+                <Link to='/'>Home</Link>
+                <div className='flex items-center'>
+                    <p>Catalog</p>
+                    <RiArrowDropDownLine className='text-2xl'></RiArrowDropDownLine>
+                </div>
 
-                </ul>
+                <Link to='/blog'>Blog</Link>
             </div>
 
-            {/* navbar right end */}
-            <div className="navbar-end">
-                <ul className="menu menu-horizontal pr-5">
-                    <li className='flex text-2xl'>
-                        <details>
-                            <summary><FaUser></FaUser></summary>
-                            <ul className="bg-base-100 text-accent rounded-sm text-lg p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    {
-                        mode === 'dark' ?
-                            <li className={`menu-option-${mode}-mode text-2xl`} onClick={() => setMode('light')}>
-                                <a><IoSunny></IoSunny></a>
-                            </li>
-                            :
-                            <li className={`menu-option-${mode}-mode text-2xl`} onClick={() => setMode('dark')}>
-                                <a><MdDarkMode></MdDarkMode></a>
-                            </li>
-                    }
-
-                </ul>
-
+            {/* navbar right */}
+            <div className='flex items-center gap-5 text-2xl'>
+                <div className='flex items-center'>
+                    <FaUser className='text-3xl'></FaUser>
+                    <RiArrowDropDownLine></RiArrowDropDownLine>
+                </div>
+                {
+                    mode === 'dark' ?
+                        <div onClick={() => setMode('light')}>
+                            <IoSunny></IoSunny>
+                        </div>
+                        :
+                        <div onClick={() => setMode('dark')}>
+                            <MdDarkMode></MdDarkMode>
+                        </div>
+                }
             </div>
-
-
         </div>
     );
 };
